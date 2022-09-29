a909be076d65bf1369dc53f8c53830307826fe4b
StasySmile <anastasiia.polieshko@gmail.com>
2022-09-29 17:48:20 +0300

    added forecast api

--- src/index.js
+++ src/index.js
@@ -81,6 +81,8 @@ function showTemperature(response) {
   windNow.innerHTML = `${wind}`;
   cityNow.innerHTML = `${city}`;
   weatherDescription.innerHTML = `${description}`;
+
+  getForecast(response.data.coord);
 }
 function showPosition(position) {
   let lat = position.coords.latitude;
@@ -110,7 +112,15 @@ function showCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-function displayForecast() {
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "1dbf926d3b4417bf379db7043bec1047";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = ``;
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
@@ -136,7 +146,6 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + ``;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
 let cityForm = document.querySelector("#city-form");
